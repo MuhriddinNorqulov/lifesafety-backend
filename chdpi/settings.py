2d350d498730cdb7a665fbd24066bf9e1747a634
muxriddin <muxriddin.developer@gmail.com>
2023-09-20 00:33:18 +0500

    postgresql

--- chdpi/settings.py
+++ chdpi/settings.py
@@ -5,7 +5,9 @@ dotenv.load_dotenv()
 
 debug = os.environ.get("DEBUG", False) == 'True'
 DEBUG = debug
-if debug:
-    from .settings_debug import *
-else:
-    from .settings_release import *
+
+from .settings_release import *
+# if debug:
+#     from .settings_debug import *
+# else:
+#     from .settings_release import *
