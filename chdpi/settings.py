85fa5968c6fac39f47ff4a5f48428cec98fccb49
muxriddin <muxriddin.developer@gmail.com>
2024-01-04 20:42:41 +0500

    added db

--- chdpi/settings.py
+++ chdpi/settings.py
@@ -3,11 +3,11 @@ import dotenv
 
 dotenv.load_dotenv()
 
-debug = os.environ.get("DEBUG", False) == 'True'
+# debug = os.environ.get("DEBUG", False) == 'True'
+debug = True
 DEBUG = debug
 
-from .settings_release import *
-# if debug:
-#     from .settings_debug import *
-# else:
-#     from .settings_release import *
+if debug:
+    from .settings_debug import *
+else:
+    from .settings_release import *
